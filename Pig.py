0766f98866c7ae88b32541ebb6780c934c02ac78
AkilDikshanYatagampitiya <akildikshanyatagampitiya@gmail.com>
2024-12-18 11:08:07 +0530

    Imported random module to get a random number between 1 to 6 and asked user to enter number of users and there names.

---
+++ Pig.py
@@ -0,0 +1,21 @@
+import random
+
+def roll():
+    min_value=1
+    max_value=6
+
+    roll=random.randint(min_value,max_value)
+    return roll
+def players():
+    num_players=int(input("Enter the number of players :"))
+    player_names=[]
+
+    for i in range(num_players):
+        name=input(f"Enter the name of player{i+1} :")
+        player_names.append(name)
+        
+    return player_names
+        
+    
+print(players())
+
